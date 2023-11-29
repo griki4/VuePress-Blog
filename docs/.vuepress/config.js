689b824e90dcc33084605545f6848dc1807f0618
griki4 <15215066996@163.com>
2023-11-29 15:32:40 +0800

    add: Next.js缓存机制

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -133,7 +133,8 @@ module.exports = {
                     {title: "Next.js官方教程", path: "/framework/next/01_Next官方教程"},
                     {title: "Next Routing", path: "/framework/next/02_Next-Routing"},
                     {title: "数据请求", path: "/framework/next/03_数据请求"},
-                    {title: "渲染策略", path: "/framework/next/04_渲染"}
+                    {title: "渲染策略", path: "/framework/next/04_渲染"},
+                    {title: "缓存机制", path: "/framework/next/05_缓存策略"}
                 ]
             }
         ]
